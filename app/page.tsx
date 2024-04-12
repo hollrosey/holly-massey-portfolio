c7eb53af8a1ebfdaca9f42d7401e1fd94e6d2f41
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-04-12 11:07:28 +0100

    removed tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main>
+      <div>
         <h1>Hello, World!</h1>
       </div>
     </main>
