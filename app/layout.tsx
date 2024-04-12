3c5e45c2f855bf5213a48bb83e0af31badd7fd24
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-04-12 23:16:27 +0100

    font applied

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from 'next/head';
 import "./globals.css";
 
 
@@ -13,8 +14,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <>
     <html lang="en">
+      <Head>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+  </Head>
       <body>{children}</body>
     </html>
+    </>
   );
 }
