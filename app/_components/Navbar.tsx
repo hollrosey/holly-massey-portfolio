e346a5ab885d6c8cacfdfee6b0c8b305b2afaee8
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-04-15 17:33:50 +0100

    started to construct navbar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,6 +1,20 @@
+import "../globals.css";
+
 export default function Navbar() {
-    return (
-      
-    );
-  }
-  
+  return (
+    <ul className="navUl">
+      <li className="navLi">
+        <a className="navAtag" href="">Contact</a>
+      </li>
+      <li className="navLi">
+        <a className="navAtag" href="">Projects</a>
+      </li>
+      <li className="navLi">
+        <a className="navAtag" href="">About</a>
+      </li>
+      <li className="navLi">
+        <a className="navAtag" href="">Home</a>
+      </li>
+    </ul>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Navbar from "./_components/Navbar";
+
 export default function Home() {
   return (
-    
+    <Navbar />
   );
 }
