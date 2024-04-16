44e50222009b024bba4126f79c7375d703b57095
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-04-16 20:30:46 +0100

    navbar styling separated

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+        <h3>Footer test</h3>
+    </footer>
+  );
+}
+
+export default Footer;
+
--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import "../globals.css";
+import "./Navbar.css";
 import React from 'react';
 import Link from 'next/link';
 
