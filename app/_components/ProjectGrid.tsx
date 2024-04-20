6c5840c7537fb722da8e63d115afaba2a96e5500
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-04-20 15:08:01 +0100

    project grid component setup

---
+++ app/_components/ProjectGrid.tsx
@@ -0,0 +1,23 @@
+"use client";
+import "./ProjectGrid.css";
+import React from 'react';
+import Link from 'next/link';
+
+interface Link {
+  name: string;
+  location: string;
+}
+
+interface NavbarProps {
+  links: Link[];
+}
+
+const ProjectGrid: React.FC<NavbarProps> = () => {
+  return (
+    
+  );
+}
+
+
+export default ProjectGrid;
+
