9636426efaae29258f0f7d297acb628bd00e4b2c
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-04-20 15:50:10 +0100

    project grid placeholder

--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -1,9 +1,13 @@
 import "../globals.css";
 import React from 'react';
+import ProjectGrid from "../_components/ProjectGrid";
 
 export default function Projects() {
     return (
+      <>
       <h1>Project test!</h1>
+      <ProjectGrid  />
+      </>
     );
   }
   
--- app/_components/ProjectGrid.tsx
+++ app/_components/ProjectGrid.tsx
@@ -1,7 +1,8 @@
 "use client";
 import "./ProjectGrid.css";
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 interface Link {
   name: string;
@@ -14,10 +15,54 @@ interface NavbarProps {
 
 const ProjectGrid: React.FC<NavbarProps> = () => {
   return (
-    
-  );
-}
+    <>
+      <div className="projectsContainer">
+        <ul className="projectGallery">
+          <li className="project">
+            <Image src="/placeholder.jpg" height="200" width="250" alt="" className="projectImg"/>
+            <div className="hoverOverlay">
+              <span className="projectName">Project name</span>
+            </div>
+          </li>
 
+          <li className="project">
+            <Image src="/placeholder.jpg" height="200" width="250" alt="" className="projectImg"/>
+            <div className="hoverOverlay">
+              <span className="projectName">Project name</span>
+            </div>
+          </li>
+
+          <li className="project">
+            <Image src="/placeholder.jpg" height="200" width="250" alt="" className="projectImg"/>
+            <div className="hoverOverlay">
+              <span className="projectName">Project name</span>
+            </div>
+          </li>
+
+          <li className="project">
+            <Image src="/placeholder.jpg" height="200" width="250" alt="" className="projectImg"/>
+            <div className="hoverOverlay">
+              <span className="projectName">Project name</span>
+            </div>
+          </li>
+
+          <li className="project">
+            <Image src="/placeholder.jpg" height="200" width="250" alt="" className="projectImg"/>
+            <div className="hoverOverlay">
+              <span className="projectName">Project name</span>
+            </div>
+          </li>
+
+          <li className="project">
+            <Image src="/placeholder.jpg" height="200" width="250" alt="" className="projectImg"/>
+            <div className="hoverOverlay">
+              <span className="projectName">Project name</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default ProjectGrid;
-
