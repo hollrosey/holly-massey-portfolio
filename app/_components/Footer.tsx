8c3ba0739724a00d2ba930ee32202cf422347dee
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-04-20 09:39:58 +0100

    added footer links (temporary before icons)

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -5,7 +5,18 @@ import "./Footer.css";
 const Footer: React.FC = () => {
   return (
     <footer>
-        <h3>Footer test</h3>
+        <h3>Get in touch</h3>
+        <ul className="underline-style">
+          <li>
+            <Link className="footerLink" href="https://github.com/hollrosey" target="_blank">GitHub</Link>
+          </li>
+          <li>
+            <Link className="footerLink" href="https://linkedin.com/in/holly-massey-6114a92a6" target="_blank">LinkedIn</Link>
+            </li>
+            <li>
+              <Link className="footerLink" href="mailto:hollyrmassey@yahoo.co.uk" target="_blank">Email</Link>
+            </li>
+        </ul>
     </footer>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 import "./globals.css";
 import Navbar from "./_components/Navbar";
 import Footer from "./_components/Footer";
@@ -11,12 +11,11 @@ export const metadata: Metadata = {
 };
 
 const navLinks = [
-  { name: "Contact", location: "/ContactForm"},
-  { name: "Projects", location: "/Projects"},
-  { name: "About", location: "/AboutMe"},
-  { name: "Home", location: "/"}
-  
-]
+  { name: "Contact", location: "/ContactForm" },
+  { name: "Projects", location: "/Projects" },
+  { name: "About", location: "/AboutMe" },
+  { name: "Home", location: "/" },
+];
 
 export default function RootLayout({
   children,
@@ -25,15 +24,21 @@ export default function RootLayout({
 }>) {
   return (
     <>
-    <html lang="en">
-      <body>
-        <main>
-        <Navbar links={navLinks} />
-          {children}
-          <Footer />
-        </main>
+      <html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          />
+        </Head>
+        <body>
+          <main>
+            <Navbar links={navLinks} />
+            {children}
+            <Footer />
+          </main>
         </body>
-    </html>
+      </html>
     </>
   );
 }
