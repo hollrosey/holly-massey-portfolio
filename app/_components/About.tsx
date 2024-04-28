3ba582f94152f4285ef8e191d14d4b09c2ac20dd
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-04-28 14:32:01 +0100

    font update

--- app/_components/About.tsx
+++ app/_components/About.tsx
@@ -15,7 +15,7 @@ const About: React.FC = () => {
           className="profilePicture"
         />
         <div className="aboutMeText">
-        <h2>I&apos;m a junior Web Developer with an eye for design</h2>
+        <h2 className="titleFont">I&apos;m a junior Web Developer with an eye for design</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
       </div>
       </div>
