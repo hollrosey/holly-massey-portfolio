2ec4c6307d61fa6ecdd41c0cea65d471a620ec5c
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-05-17 16:44:23 +0100

    required fields, error handling working, success handling working, loading working

--- app/_components/Form.tsx
+++ app/_components/Form.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { ChangeEvent, useState } from "react";
 import "./Form.css";
 
@@ -10,113 +10,131 @@ const Form: React.FC = () => {
     subject: "",
     message: "",
   });
+
   const [formStatus, setFormStatus] = useState("Pending...");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
-    setFormStatus("Pending..."); 
-  
+
+    setIsSubmitting(true);
+    setFormStatus("Pending...");
+
     try {
       const response = await fetch("https://formspree.io/f/xpzvnerd", {
         method: "POST",
         body: JSON.stringify(formData),
         headers: { "Content-Type": "application/json" },
       });
-  
+
       if (response.ok) {
-        setFormStatus("Success!"); // Set status to "success" on success
-        setFormData({ firstName: "", lastName: "", email: "", subject: "", message: "" }); // Reset form data
+        setFormStatus("success");
+        setFormData({ firstName: "", lastName: "", email: "", subject: "", message: "" });
       } else {
-        setFormStatus("error"); // Set status to "error" on failure
-        console.error("Error submitting form:", response); // Log the error
+        setFormStatus("error");
+        console.error("Error submitting form:", response);
       }
     } catch (error) {
-      setFormStatus("error"); // Set status to "error" on any other error
-      console.error("Error submitting form:", error); // Log the error
+      setFormStatus("error");
+      console.error("Error submitting form:", error);
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
   interface FormEventWithTarget extends EventTarget {
     target: HTMLInputElement;
   }
-  
+
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setFormData({
       ...formData,
       [(e.target as HTMLInputElement).name]: (e.target as HTMLInputElement).value,
     });
   };
-  
+
   return (
     <>
       <div className="bodyContainer">
         <div className="formContainer">
-          <form onSubmit={(e) => handleSubmit(e)} action="https://formspree.io/f/xpzvnerd" method="POST">
+          <span className="reqText">An <span className="required">*</span> indicates a required field.</span>
+          <form onSubmit={handleSubmit} action="https://formspree.io/f/xpzvnerd" method="POST">
             <div className="nameFields">
-              <label htmlFor="firstName">First Name</label>
+              <label htmlFor="firstName">First Name<span className="required">*</span></label>
               <input
                 type="text"
                 id="firstName"
                 name="firstName"
                 placeholder="Enter your first name"
                 value={formData.firstName}
-                onChange={(e) => handleChange(e)}
+                onChange={handleChange}
               />
 
-              <label htmlFor="lastName">Last Name</label>
+              <label htmlFor="lastName">Last Name<span className="required">*</span></label>
               <input
                 type="text"
                 id="lastName"
                 name="lastName"
                 placeholder="Enter your last name"
                 value={formData.lastName}
-                onChange={(e) => handleChange(e)}
+                onChange={handleChange}
               />
             </div>
 
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">Email<span className="required">*</span></label>
             <input
               type="email"
               id="email"
               name="email"
               placeholder="email@address.com"
               value={formData.email}
-              onChange={(e) => handleChange(e)}
+              onChange={handleChange}
             />
 
-            <label htmlFor="subject">Subject</label>
+            <label htmlFor="subject">Subject<span className="required">*</span></label>
             <input
               type="text"
               id="subject"
               name="subject"
               placeholder="Brief description of your message"
               value={formData.subject}
-              onChange={(e) => handleChange(e)}
+              onChange={handleChange}
             />
 
-            <label htmlFor="message">Message</label>
+            <label htmlFor="message">Message<span className="required">*</span></label>
             <textarea
               id="message"
               name="message"
               placeholder="Enter your message here"
               value={formData.message}
-              onChange={(e) => handleChange(e)}
+              onChange={handleChange}
             ></textarea>
             <div className="buttonContainer">
               <button type="submit">Send</button>
             </div>
           </form>
-          {formStatus === "success" && (
-      <div className="success-message">Thank you for your message, I&apos;ll get back to you shortly!</div>
-    )}
-    {formStatus === "error" && (
-      <div className="error-message">Error submitting form. Please try again.</div>
-    )}
+          {isSubmitting && (
+            <div className="loading">
+              <p>Submitting...</p>
+            </div>
+          )}
+          {formStatus === "success" && ( // Success message based on formStatus
+            <div className="successMessage">
+              <p>Thank you for your message, I&apos;ll get back to you shortly!</p>
+            </div>
+          )}
+          {formStatus === "error" && (
+            <div className="errorMessage">
+              <p>Error submitting form. Please try again.</p>
+            </div>
+          )}
         </div>
       </div>
+
     </>
   );
 };
 
 export default Form;
+
+
