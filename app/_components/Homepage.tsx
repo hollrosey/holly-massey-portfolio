22f86df010bbc0cc5025e8eb1199baa66e40cd6d
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-05-17 08:09:01 +0100

    toolkkit

--- app/_components/Homepage.tsx
+++ app/_components/Homepage.tsx
@@ -24,26 +24,29 @@ const Homepage: React.FC = () => {
         </Link>
       </div>
 
-      <div className="workTogether text">
-        <h2 className="text titleFont">Let&apos;s work together!</h2>
-        <Link className="getInTouch text" href="/ContactForm">
-          <p>Get in touch</p>
-        </Link>
-      </div>
-      <div className="toolkit">
-        <h3>Languages and Tools</h3>
+      <div className="toolkit text">
+        <h2 className="text titleFont">Languages and Tools</h2>
         <div className="toolIcons">
         <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" alt="" height="50" width="50"/>
     <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" alt="" height="50" width="50" />
     <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" alt="" height="50" width="50"/>
     <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" alt="" height="50" width="50"/>
     <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" alt="" height="50" width="50"/>
+    <br />
     <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" alt="" height="50" width="50" />
     <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" alt="" height="50" width="50" />    
     <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" alt="" height="50" width="50" />
     <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" alt="" height="50" width="50" />
     <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/canva/canva-original.svg" alt="" height="50" width="50" />
         </div>
+        
+        
+      </div>
+      <div className="workTogether">
+        <h2 className="text titleFont">Let&apos;s work together!</h2>
+        <Link className="getInTouch text" href="/ContactForm">
+          <p>Get in touch</p>
+        </Link>
       </div>
     </>
   );
